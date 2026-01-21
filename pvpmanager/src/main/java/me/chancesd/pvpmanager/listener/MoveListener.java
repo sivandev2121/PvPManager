308a2abc8a27fc53c1a3f81454c496087c0628fc
sivandev2121 <sporhubbb@gmail.com>
2026-01-21 23:50:21 +0300

    MoveListener.java Da Optimasyon Yapıldı

--- pvpmanager/src/main/java/me/chancesd/pvpmanager/listener/MoveListener.java
+++ pvpmanager/src/main/java/me/chancesd/pvpmanager/listener/MoveListener.java
@@ -2,17 +2,15 @@ package me.chancesd.pvpmanager.listener;
 
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
-
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-
 import me.chancesd.pvpmanager.manager.DependencyManager;
 import me.chancesd.pvpmanager.manager.PlayerManager;
 import me.chancesd.pvpmanager.player.CombatPlayer;
@@ -21,38 +19,50 @@ import me.chancesd.pvpmanager.setting.Lang;
 
 public class MoveListener implements Listener {
 
-	private final PlayerManager ph;
-	private final DependencyManager depManager;
-	private final Cache<UUID, Player> cache = CacheBuilder.newBuilder().weakValues().expireAfterWrite(1, TimeUnit.SECONDS).build();
-	private final double pushbackForce = Conf.PUSHBACK_FORCE.asDouble();
+    private final PlayerManager ph;
+    private final DependencyManager depManager;
+    private final Cache<UUID, Player> cache = CacheBuilder.newBuilder().weakValues().expireAfterWrite(1, TimeUnit.SECONDS).build();
+    private final double pushbackForce = Conf.PUSHBACK_FORCE.asDouble();
 
-	public MoveListener(final PlayerManager ph) {
-		this.ph = ph;
-		this.depManager = ph.getPlugin().getDependencyManager();
-	}
+    public MoveListener(final PlayerManager ph) {
+        this.ph = ph;
+        this.depManager = ph.getPlugin().getDependencyManager();
+    }
 
-	@EventHandler(ignoreCancelled = true)
-	public final void onPlayerMove(final PlayerMoveEvent event) {
-		final Location locTo = event.getTo();
-		final Location locFrom = event.getFrom();
-		if (locTo == null || locFrom.getBlockX() == locTo.getBlockX() && locFrom.getBlockZ() == locTo.getBlockZ()
-				&& locFrom.getBlockY() == locTo.getBlockY())
-			return;
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public final void onPlayerMove(final PlayerMoveEvent event) {
+        final Location locTo = event.getTo();
+        if (locTo == null) return;
+        
+        final Location locFrom = event.getFrom();
 
-		final Player player = event.getPlayer();
-		final CombatPlayer pvplayer = ph.get(player);
-		if (!pvplayer.isInCombat())
-			return;
+        // AGIR OPTIMIZASYON: Oyuncu blok bazında yer degistirmediyse (kafasini cevirdiyse) aninda dur!
+        if (locFrom.getBlockX() == locTo.getBlockX() && 
+            locFrom.getBlockZ() == locTo.getBlockZ() && 
+            locFrom.getBlockY() == locTo.getBlockY()) {
+            return;
+        }
 
-		if (!depManager.canAttackAt(null, locTo) && depManager.canAttackAt(null, locFrom)) {
-			final Vector newVel = locFrom.toVector().subtract(locTo.toVector());
-			newVel.setY(newVel.getY() + 0.1).normalize().multiply(pushbackForce);
-			player.setVelocity(newVel);
-			if (!cache.asMap().containsKey(event.getPlayer().getUniqueId())) {
-				pvplayer.message(Lang.PUSHBACK_WARNING);
-				cache.put(player.getUniqueId(), player);
-			}
-		}
-	}
+        // AGIR OPTIMIZASYON 2: Savasat olmayan oyuncu icin hesaplama yapma (CPU tasarrufu)
+        final CombatPlayer pvplayer = ph.get(event.getPlayer());
+        if (!pvplayer.isInCombat()) {
+            return;
+        }
 
+        // Guvenli bolge ve itme (pushback) kontrolü
+        if (!depManager.canAttackAt(null, locTo) && depManager.canAttackAt(null, locFrom)) {
+            final Player player = event.getPlayer();
+            final Vector newVel = locFrom.toVector().subtract(locTo.toVector());
+            
+            // Itme gucunu hesapla ve uygula
+            newVel.setY(newVel.getY() + 0.1).normalize().multiply(pushbackForce);
+            player.setVelocity(newVel);
+
+            // Oyuncuya uyari mesajini gonder (Cache ile spam engelli)
+            if (!cache.asMap().containsKey(player.getUniqueId())) {
+                pvplayer.message(Lang.PUSHBACK_WARNING);
+                cache.put(player.getUniqueId(), player);
+            }
+        }
+    }
 }
